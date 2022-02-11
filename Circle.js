64520922436e6b84f6dcb19a2e340dff77160c61
T-H-P-G <98300722+T-H-P-G@users.noreply.github.com>
2022-02-11 15:23:49 +0530

    Add files via upload

---
+++ Circle.js
@@ -0,0 +1,15 @@
+
+let Shape = require('./shape')
+
+
+export class Circle extends Shape {
+    constructor(color) {
+        super(color);
+    }
+    calculateArea() {
+        console.log(`${this.color}  area is calculated`)
+
+    }
+}
+
+module.exports = Circle
